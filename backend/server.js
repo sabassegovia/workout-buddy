6d22bf9f878174329d6b352057970b393813418c
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-09 00:28:07 -0700

    connecting to db and listening on port

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 const express = require('express');
 require('dotenv').config();
-
-////////Express app and port////////////
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 6900;
 const workoutRoutes = require('./routes/workouts.js');
 
 
+///////////MIDDLEWARE///////////
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 app.use(express.json());
 
-///////////ROUTES/////////////////////
+//////////CONNECT TO DB////////////
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on ${PORT},\nConnected to MongoDB`);
+    });
+  })
+  .catch((err) => {console.log(err)})
+
+
+  ///////////ROUTES/////////////////////
 app.use('/api/workouts',workoutRoutes)
 
-/////////listen for requests///////////////
-app.listen(PORT, () => {
-  console.log(`Running on ${PORT}`);
-});
 
