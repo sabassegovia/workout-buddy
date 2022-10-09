b1bc4718dbae246612f2801a4851fea117b1eba0
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-08 23:43:56 -0700

    setting up api endpoints

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,25 @@ app.get('/', (req, res) => {
   res.json({ response: "Welcome to the app" });
 })
 
+app.get('/workouts', (req, res) => {
+  res.json({ response: "Welcome to the app" });
+})
+
+app.post('/workouts', (req, res) => {
+  res.json({ response: "Welcome to the app" });
+})
+
+app.get('/workouts/:id', (req, res) => {
+  res.json({ response: "Welcome to the app" });
+})
+
+app.delete('/workouts/:id', (req, res) => {
+  res.json({ response: "Welcome to the app" });
+})
+
+app.patch('/workouts/:id', (req, res) => {
+  res.json({ response: "Welcome to the app" });
+})
 
 /////////listen for requests///////////////
 app.listen(PORT, () => {
