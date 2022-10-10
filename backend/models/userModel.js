4305deece3c24b2d697dea56d8e7a8b6904083c2
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-10 14:36:01 -0700

    complete the loginUser controller and model method on backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -39,4 +39,20 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 }
 
+//Static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw Error("All fields must be filled");
+
+
+  const user = await this.findOne({ email });
+  if (!user) throw Error("Incorrect email");
+
+  //compares the input password with the saved hash password
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) throw Error("Password incorrect");
+
+  return user;
+}
+
 module.exports = mongoose.model('User', userSchema);
