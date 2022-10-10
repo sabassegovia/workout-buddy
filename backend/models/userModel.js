b8e221e27765119f4eead0e6e668ecab190a1531
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-10 10:48:53 -0700

    complete a basic sign up request on back end. Salt/Hash incorporated in userModel. Tested on Postman

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -14,5 +15,22 @@ const userSchema = new Schema({
     required: true
   },
 }, { timestamps: true });
+
+//Static signup Method
+userSchema.statics.signup = async function(email, password) {
+  //first check if email already exists in DB
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email is already registered");
+  }
+
+  //Salting password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+}
 
 module.exports = mongoose.model('User', userSchema);
