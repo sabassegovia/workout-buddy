0dbca1e8037e7ef26d622ffdecf88647fb5d80ca
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-09 17:18:16 -0700

    complete the front end starter set up.

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails.js";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts');
+      const data = await response.json();
+
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    }
+    fetchWorkouts();
+  }, []);
+  //empty array, only fires once upon render
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home;
