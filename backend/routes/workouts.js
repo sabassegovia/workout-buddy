8610082e077db94f376b30765b30e847b7172e38
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-10 23:26:06 -0700

    add middleware folder. Authentication in middleware folder to protect workout routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getAllWorkouts, getSingleWorkout, createWorkout, deleteWorkout, updateWorkout} = require('../controllers/workoutController.js');
+const requireAuth = require('../middleware/requireAuth.js');
+
+//require auth for all workout routes
+router.use(requireAuth);
 
 router.get('/', getAllWorkouts);
 router.get('/:id', getSingleWorkout);
